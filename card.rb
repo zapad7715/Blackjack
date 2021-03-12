07b39d98dbe619196eb85eaa669e92ebc204d6b3
aleksey <zapad7715@ya.ru>
2021-03-12 08:52:47 +0300

    Deck and cart

---
+++ card.rb
@@ -0,0 +1,16 @@
+class Card
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
+
+  def value
+    return 10 if %w[J Q K].include?(@value)
+    return 11 if ['A'].include?(@value)
+    @value
+  end
+
+  def to_s
+    "#{@value} #{@suit}"
+  end
+end
